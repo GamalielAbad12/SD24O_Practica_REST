b7a3641980cfc4901fc7aff2e747bef36344b7cd
GamalielAbad12 <gamaliel.abad@cua.uam.mx>
2024-12-08 16:32:43 -0600

    Problemas con el repo

--- api.py
+++ api.py
@@ -18,31 +18,44 @@ def obtener_alumnos(sesion:Session=Depends(generador_sesion)):
     print("Api hace una consulta de usuarios")
     return repo.obtener_alumnos(sesion) 
 
-
-
 @app.get("/alumnos/{id}")
 def obtener_alumnos_id(id:int, sesion: Session=Depends(generador_sesion)):
     print("Api hace una consulta de usuarios por id")
-    return repo.obtener_alumnos(id,sesion)
+    return repo.obtener_alumnos_id(id,sesion)
 
-"""
+
 @app.get("/alumnos/{id}/calificaciones")
-def obtener_calificaciones_alumnos_id(id:int):
+def obtener_calificaciones_alumnos_id(id:int, sesion: Session=Depends(generador_sesion)):
+    print("Api hace una consulta de las calificaciones por id de alumno")
+    return repo.obtener_calificaciones_alumnos_id(id, sesion)
+
 
 @app.get("/alumnos/{id}/fotos")
-def obtener_fotos_alumnos_id(id:int):
-    
+def obtener_fotos_alumnos_id(id:int, sesion:Session=Depends(generador_sesion)):
+    print("Api hace una consulta de las fotos del alumno por id de alumno")
+    return repo.obtener_fotos_alumnos_id(id, sesion)
+
+
 @app.get("/fotos/{id}")
-def obtener_fotos_id(id:int):
+def obtener_fotos_id(id:int, sesion:Session=Depends(generador_sesion)):
+    print("Api hace una consulta de las fotos con el id de la foto")
+    return repo.obtener_fotos_id(id, sesion)
+
 
 @app.get("/calificaciones/{id}")
-def obtener_calificaciones_id(id:int):
+def obtener_calificaciones_id(id:int, sesion:Session=Depends(generador_sesion)):
+    print("Api hace una consulta de las calificaciones con el id de la calificacion")
+    return 
 
+
+"""
 @app.delete("/fotos/{id}")
 def eliminar_fotos_id(id:int):
 
 @app.delete("/calificaciones/{id}")
 def eliminar_clasificaciones_id(id:int):
+
+@app.delete("/alumnos/{id}/calificaciones)
 
 @app.delete("/alumnos/{id}/fotos")
 def eliminar_fotos_alumnos_id(id:int):
--- orm/repo.py
+++ orm/repo.py
@@ -2,12 +2,42 @@ import orm.modelos as modelos
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 
+#SELECT * FROM app.alumnos
 def obtener_alumnos(sesion:Session):
-    print("select * from app.usuarios")
+    print("SELECT * FROM app.alumnos")
     return sesion.query(modelos.Alumno).all()
-    
-"""
-def obtener_alumnos_id():
 
-def obtener_calificaciones_alumnos_id():
-"""
+#SELECT * FROM app.alumnos WHERE id={id_al}
+def obtener_alumnos_id(id:int, sesion:Session):
+    print("SELECT * FROM app.alumnos WHERE id=id_al")
+    return sesion.query(modelos.Alumno).filter(modelos.Alumno.id==id).first()
+
+#SELECT * FROM app.fotos
+def obtener_fotos(sesion:Session):
+    print("SELECT * FROM app.fotos")
+    return sesion.query(modelos.Foto).all()
+
+#SELECT * FROM app.fotos WHERE id={id_fo}
+def obtener_fotos_id(id:int, sesion:Session):
+    print("SELECT * FROM app.fotos WHERE id=id_fo")
+    return sesion.query(modelos.Foto).filter(modelos.Foto.id==id).first()
+
+#SELECT * FROM app.fotos WHERE id_alumnos={id_al}
+def obtener_calificaciones_alumnos_id(id:int, sesion:Session):
+    print("SELECT * FROM app.calificaciones WHERE id_alumnos=id_al")
+    return sesion.query(modelos.Calificacion).filter(modelos.Calificacion.id_alumno==id).all()
+
+#SELECT * FROM app.calificaciones
+def obtener_calificaciones(sesion:Session):
+    print("SELECT * FROM app.calificaciones")
+    return sesion.query(modelos.Calificacion).all()
+
+#
+def obtener_fotos_alumnos_id(id:int, sesion:Session):
+    print("SELECT * FROM app.fotos WHERE id_alumnos={id_al}")
+    return sesion.query(modelos.Foto).filter(modelos.Foto.id_alumno==id).all()
+
+#SELECT * FROM app.calificaciones WHERE id=alumno}
+def obtener_calificaciones_id(id:int, sesion:Session):
+    print("SELECT * FROM app.calificaciones WHERE id=id_alumno")
+    return sesion.query(modelos.Calificacion.id)
