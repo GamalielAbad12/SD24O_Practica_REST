a80a69fff78595a7eabe6820577f58f73edfd959
GamalielAbad12 <gamaliel.abad@cua.uam.mx>
2024-12-11 08:36:38 -0600

    Se crearon los esquemas

---
+++ orm/esquemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+class alumnoBase(BaseModel):
+    nombre: str
+    edad: int
+    domicilio: str 
+    carrera: str 
+    trimestre: str
+    email: str
+    password: str
+
+class calificacionBase(BaseModel):
+    uea: str
+    calificacion: str
+
+class fotoBase(BaseModel):
+    titulo: str
+    descripcion: str
+    ruta: str
